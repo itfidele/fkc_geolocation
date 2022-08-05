a53c3f0467654596e33e2c98e4f1cb5fe9944a5f
Fidele K.Cyisa <fihacker000@gmail.com>
2022-08-05 17:06:01 +0300

    getting client ip over proxy

--- location/views.py
+++ location/views.py
@@ -7,7 +7,7 @@ def get_client_ip(request):
     x_forward_for = request.META.get("HTTP_X_FORWARDED_FOR")
     
     if x_forward_for:
-        ip = x_forward_for.split(',')[0]
+        ip = x_forward_for.split(',')[-1]
     else:
         ip = request.META.get("REMOTE_ADDR")
     
